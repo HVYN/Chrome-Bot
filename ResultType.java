7fe1e83ce6be3906714f5785715badabc3fe4eee
John Zhao <34289844+HVYN@users.noreply.github.com>
2022-06-03 23:26:06 -0500

    Redundant File

--- ResultType.java
+++
@@ -1,9 +0,0 @@
-
-//  HELPER: Enum ResultType assists
-//      in simplifying otherwise hefty information into
-//      a small, readable format.
-
-public enum ResultType
-{
-    RETURNED_HOME, SUCCESS, OUT_OF_RESOURCE
-}
